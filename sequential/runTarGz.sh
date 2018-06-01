285bb1ea57d4f254b34a3ad1ae2b4b00bb8fdc03
Yann Chemin <ychemin@gmail.com>
2018-06-01 11:36:11 +0200

    update tarball input data version

--- sequential/runTarGz.sh
+++ sequential/runTarGz.sh
@@ -5,23 +5,24 @@
 #else direct input in the script:
 n1=4
 n2=4
+root=$(pwd)
 #Go to data dir
-cd ../testdata/
+cd $root/../testdata/
 rm -Rf sub_*
 #untar each tarball at a time
 for file in *.tar.gz
 do
-	mkdir -p sub_$n1\_$n2
-	tar -C sub_$n1\_$n2 -xvf $file
-	cd sub_$n1\_$n2
-	origTGZFiles=$(ls *.tif)
-	for untarballedFile in *.tif
+	mkdir -p $root/../testdata/sub_$n1\_$n2
+	listF=$(tar -tzf lotsofdata.tar.gz | grep .tif)
+	for listedFile in listF
 	do
-		bash ../../sequential/tileimg.sh $untarballedFile $n1 $n2
+		cd $root/../testdata/
+		tar -C $root/../testdata/sub_$n1\_$n2 -xvf $file --wildcards --no-anchored $listedFile
+		cd $root/../testdata/sub_$n1\_$n2
+		bash $root/tileimg.sh $listedFile $n1 $n2 
 	done
-	#Cleanup $origTGZFiles
-	for oTGZf in $origTGZFiles
-	do 
-		rm -f $oTGZf
-	done
+	rm -f $root/../testdata/sub_$n1\_$n2/*.tif
+	mv $root/../testdata/sub_$n1\_$n2/sub_$n1\_$n2/*.tif $root/../testdata/sub_$n1\_$n2/
+	rm -Rf $root/../testdata/sub_$n1\_$n2/sub_$n1\_$n2
+	cd $root/../testdata/
 done
