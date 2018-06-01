0bbf55e3c5ea47ded6366a884af76c169d46b6c8
Yann Chemin <ychemin@gmail.com>
2018-06-01 10:32:24 +0200

    update

--- sequential/run.sh
+++ sequential/run.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 cd ../testdata/
 rm -Rf sub_*
-n=4
+#if user input i.e. "bash run.sh 8 6"
+#n1=$1
+#n2=$2
+#else direct input in the script:
+n1=4
+n2=4
 for file in *.tif
 do
-	mkdir -p sub_$n\_$n
-	bash ../sequential/tileimg.sh $file $n $n
+	mkdir -p sub_$n1\_$n2
+	bash ../sequential/tileimg.sh $file $n1 $n2
 done
 
