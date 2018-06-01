71d44c6a883a684c361a3ded5738a2e757608ea7
Yann Chemin <ychemin@gmail.com>
2018-06-01 10:24:59 +0200

    update

--- sequential/run.sh
+++ sequential/run.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 cd ../testdata/
 rm -Rf sub_*
-n=2
-for (( i=1 ; i <= 1 ; i++))
+n=4
+for file in *.tif
 do
-	n=$(echo $n"+"$n | bc)
 	mkdir -p sub_$n\_$n
 	bash ../sequential/tileimg.sh LC082150672017010301T1-SC20180219142350_stack_GCS.tif $n $n
 done
